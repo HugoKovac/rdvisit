a0299c4335f234dc708986bbe6f4a54143ed64bd
Hugo Kovac <97465389+HugoKovac@users.noreply.github.com>
2026-05-13 15:53:11 +0200

    fix: create appointments for existing common users

--- db/sqlc/appointment.sql.go
+++ db/sqlc/appointment.sql.go
@@ -17,7 +17,11 @@ WITH patient AS (
     INSERT INTO users (email, firstname, lastname, password_hash, role, verified)
     VALUES ($3, '', '', '', 'patient', false)
     ON CONFLICT (email) DO UPDATE
-    SET email = EXCLUDED.email
+    SET role = CASE
+            WHEN users.role = 'common' THEN 'patient'
+            ELSE users.role
+        END,
+        updated_at = NOW()
     RETURNING id, email, role
 )
 INSERT INTO appointments (date, practitioner_id, patient_id)
